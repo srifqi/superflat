cb3b6ececf1a614f225ade2db8e7a1643aed4fa8
srifqi <muhammadrifqipriyosusanto@gmail.com>
2014-07-18 15:41:03 +0700

    Create parameter.lua
    change to text code

---
+++ parameter.lua
@@ -0,0 +1,32 @@
+------------------------
+--      Parameter     --
+-- Modify layers here --
+------------------------
+
+sflat.Y_ORIGIN = 1
+sflat.BLOCKS = "superflat:bedrock=1,default:dirt=2,default:dirt_with_grass=1"
+
+--[[
+
+--- DEFAULT SUPERFLAT
+	"superflat:bedrock=1,default:dirt=2,default:dirt_with_grass=1"
+
+-- SUPERFLAT: Shallow Underground
+	"superflat:bedrock=1,default:stone=230,default:dirt=5,default:dirt_with_grass=1"
+
+-- SUPERFLAT: Deep Underground
+	"superflat:bedrock=1,default:stone=920,default:dirt=10,default:dirt_with_grass=1"
+
+-- SUPERFLAT: Very Deep Underground
+	"superflat:bedrock=1,default:stone=3680,default:dirt=15,default:dirt_with_grass=1"
+
+-- SUPERFLAT: Bottomless Pit
+	"default:cobble=2,default:dirt=3,default:dirt_with_grass=1"
+
+-- DESERT SUPERFLAT
+	"superflat:bedrock,default:desert_sand=3"
+
+-- SUPERFLAT: Farmer's dream
+	"superflat:bedrock=1,default:water_source=1,farming:soil_wet=1"
+
+--]]
