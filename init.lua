0e63218efa758d355af2a6328d64ac29c5509caa
srifqi <muhammadrifqipriyosusanto@gmail.com>
2014-07-18 15:39:35 +0700

    Update init.lua
    change to text code

--- init.lua
+++ init.lua
@@ -1,21 +1,14 @@
 -- (Yet Another) Superflat Map Generator
-
----------------
--- Parameter --
----------------
-
-local Y_ORIGIN = 1
-local BLOCKS = {
-	"superflat:bedrock", -- what i want is real bedrock :(
-	"default:dirt",
-	"default:dirt",
-	"default:dirt_with_grass"
-}
-
+-- Modify layers in parameter.lua !
 
 --------------------
 -- Internal stuff --
 --------------------
+
+sflat = sflat or {}
+
+dofile(minetest.get_modpath("superflat").."/parsetext.lua")
+dofile(minetest.get_modpath("superflat").."/parameter.lua")
 
 minetest.register_on_mapgen_init(function(mgparams)
 		minetest.set_mapgen_params({mgname="singlenode"})
@@ -33,7 +26,8 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
 	local data = vm:get_data()
-	if minp.y > Y_ORIGIN + #BLOCKS then
+	local pBLOCKS = sflat.parsetext(sflat.BLOCKS)
+	if minp.y > sflat.Y_ORIGIN + #pBLOCKS then
 		return
 	end
 	for z = minp.z, maxp.z do
@@ -41,8 +35,8 @@ minetest.register_on_generated(function(minp, maxp, seed)
 		local yy = 1
 	for y = minp.y, maxp.y do
 		local vi = area:index(x, y, z)
-		if (y >= Y_ORIGIN and y < Y_ORIGIN + #BLOCKS) then
-			data[vi] = minetest.get_content_id(BLOCKS[yy])
+		if (y >= sflat.Y_ORIGIN and y < sflat.Y_ORIGIN + #pBLOCKS) then
+			data[vi] = minetest.get_content_id(pBLOCKS[yy])
 			yy = yy + 1
 		else
 			data[vi] = minetest.get_content_id("air")
@@ -56,19 +50,19 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	vm:write_to_map(data)
 end)
 
-local bedrock_timer = 0
+sflat.bedrock_timer = 0
 minetest.register_globalstep(function(dtime)
-	bedrock_timer = bedrock_timer - dtime
-	if bedrock_timer > 0 then return end
+	sflat.bedrock_timer = sflat.bedrock_timer - dtime
+	if sflat.bedrock_timer > 0 then return end
 	for k,player in ipairs(minetest.get_connected_players()) do
-		bedrock_timer = 1
+		sflat.bedrock_timer = 1
 		local pos = player:getpos()
-		if pos.y < Y_ORIGIN-1 then
+		if pos.y < sflat.Y_ORIGIN-1 then
 			-- teleport them back to surface
-			player:setpos({x=pos.x,y=#BLOCKS+2,z=pos.z})
+			player:setpos({x=pos.x,y=#pBLOCKS+2,z=pos.z})
 			-- build first layers under them
-			if minetest.env:get_node({x=pos.x,y=Y_ORIGIN,z=pos.z}).name == "air" then
-				minetest.env:set_node({x=pos.x,y=0,z=pos.z}, {name=BLOCKS[1]})
+			if minetest.env:get_node({x=pos.x,y=sflat.Y_ORIGIN,z=pos.z}).name == "air" then
+				minetest.env:set_node({x=pos.x,y=0,z=pos.z}, {name=pBLOCKS[1]})
 			end
 		end
 	end
